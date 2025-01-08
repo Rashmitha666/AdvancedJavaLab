e07e06f3f23c96d6304fc051e4af97ed2b09662d
Rashmitha Mahesh <143868841+Rashmitha666@users.noreply.github.com>
2025-01-09 00:25:59 +0530

    Update DisplayCookies.java

--- Q8/8b/DisplayCookies.java
+++ Q8/8b/DisplayCookies.java
@@ -17,39 +17,39 @@ public class displayCookies extends HttpServlet {
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException 
 	{
 		response.setContentType("text/html");
-        PrintWriter out = response.getWriter();
-        
-        Cookie cookie1 = new Cookie("Cookie1", "Value1");
-        Cookie cookie2 = new Cookie("Cookie2", "Value2");
-        Cookie cookie3 = new Cookie("Cookie3", "Value3");
-        Cookie cookie4 = new Cookie("Cookie4", "Value4");
-        
-        cookie1.setMaxAge(60); 
-        cookie2.setMaxAge(60);
-        
-        response.addCookie(cookie1);
-        response.addCookie(cookie2);
-        response.addCookie(cookie3);
-        response.addCookie(cookie4);
-        
-        Cookie[] cookies = request.getCookies();
-
-        out.println("<h2>Cookies Status</h2>");
-
-        if (cookies != null) 
-        {
-            for (Cookie cookie : cookies) 
-            {
-                out.println("<p>" + cookie.getName() + ": " + cookie.getValue() + "</p>");
-            }
-        } 
-        
-        else 
-        {
-            out.println("<p>No cookies found. Some cookies might have expired.</p>");
-        }
-
-        out.println("<p>Refresh the page to see the effect of cookie expiration.</p>");
+	        PrintWriter out = response.getWriter();
+	        
+	        Cookie cookie1 = new Cookie("Cookie1", "Value1");
+	        Cookie cookie2 = new Cookie("Cookie2", "Value2");
+	        Cookie cookie3 = new Cookie("Cookie3", "Value3");
+	        Cookie cookie4 = new Cookie("Cookie4", "Value4");
+	        
+	        cookie1.setMaxAge(60); 
+	        cookie2.setMaxAge(60);
+	        
+	        response.addCookie(cookie1);
+	        response.addCookie(cookie2);
+	        response.addCookie(cookie3);
+	        response.addCookie(cookie4);
+	        
+	        Cookie[] cookies = request.getCookies();
+	
+	        out.println("<h2>Cookies Status</h2>");
+	
+	        if (cookies != null) 
+	        {
+	            for (Cookie cookie : cookies) 
+	            {
+	                out.println("<p>" + cookie.getName() + ": " + cookie.getValue() + "</p>");
+	            }
+	        } 
+	        
+	        else 
+	        {
+	            out.println("<p>No cookies found. Some cookies might have expired.</p>");
+	        }
+	
+	        out.println("<p>Refresh the page to see the effect of cookie expiration.</p>");
 	}
 
 }
